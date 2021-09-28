3463cbff0ec3c4c3588d1fc7dd806d742edf9d7d
PracLee <mrgoga@naver.com>
2021-09-28 15:55:27 +0900

    개인정보 삭제

--- mailTest/src/mailTest/mail/Mail.java
+++ mailTest/src/mailTest/mail/Mail.java
@@ -7,8 +7,8 @@ public class Mail extends Authenticator{
 	// 각자의 구글 아이디를 입력
 	PasswordAuthentication pa;
 	public Mail() {
-		String mail_id = "bjlee.spsbsc@gmail.com";
-		String mail_pw = "2qudworne!";
+		String mail_id = "구글메일아이디입력 @gmail.com";
+		String mail_pw = "비밀번호입력";
 		pa = new PasswordAuthentication(mail_id, mail_pw);
 	}
 	public PasswordAuthentication getPasswordAuthentication() {
